626d049f62ac6de349a7b95242cccb4729888ee3
Bryant <33697753+BryCant@users.noreply.github.com>
2020-11-15 20:03:20 -0600

    GOD MODE adde

--- perkinsBCipher.py
+++ perkinsBCipher.py
@@ -38,11 +38,25 @@ elif cipherChoice.lower() == 'c':
         #   Decoding message file
         outputFile = open(outputName, 'w')
         with open(fileName) as messageFile:
-            Ciphers.encode_message(messageFile, outputFile, Ciphers.caesar_cipher(int(shiftNum)*-1))
+            Ciphers.decode_message(messageFile, outputFile, Ciphers.caesar_cipher(int(shiftNum)*-1))
         outputFile.close()
     else:
         print('Invalid input!')
         exit()
+# GOD MODE
+elif cipherChoice.lower() == 'g':
+    shiftNum = input("What is the caesar shift magnitude? ")
+    outputFile = open(outputName, 'w') if fileName != outputName else open('-' + outputName, 'w')
+    with open(fileName) as messageFile:
+        Ciphers.qwerty_cipher(messageFile, outputFile, 'e')
+        Ciphers.encode_message(messageFile, outputFile, Ciphers.caesar_cipher(int(shiftNum)))
+        Ciphers.atbash_cipher(messageFile, outputFile)
+        Ciphers.encode_message(messageFile, outputFile, Ciphers.caesar_cipher(int(shiftNum)))
+        Ciphers.atbash_cipher(messageFile, outputFile)
+        Ciphers.encode_message(messageFile, outputFile, Ciphers.caesar_cipher(int(shiftNum)))
+        Ciphers.atbash_cipher(messageFile, outputFile)
+        Ciphers.encode_message(messageFile, outputFile, Ciphers.caesar_cipher(int(shiftNum)))
+    outputFile.close()
 # All of the error handling
 else:
     print('Invalid input!')
