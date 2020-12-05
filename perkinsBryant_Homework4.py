76f895994770a602307f5d867f2d8ed2098fe7d1
Bryant <33697753+BryCant@users.noreply.github.com>
2020-12-05 17:22:09 -0600

    Comments added, constant rearranged, and lbs to kg more precise

--- perkinsBryant_Homework4.py
+++ perkinsBryant_Homework4.py
@@ -4,39 +4,41 @@ from tkinter import ttk
 
 class WilkesWindow:
     def __init__(self):
-        self.window = tk.Tk()
-        self.window.title("perkinsBryant_Homework4")
-        self.score = "Your Wilkes Score is "
-        self.weight_entry = tk.StringVar()
-        self.bench_entry = tk.StringVar()
-        self.squat_entry = tk.StringVar()
-        self.deadlift_entry = tk.StringVar()
+        self.window = tk.Tk()  # create window containing errthang
+        self.window.title("perkinsBryant_Homework4")  # title of window
+        self.score = "Your Wilks Score: "  # Default results text
+        self.weight_entry = tk.StringVar()  # var will store weight text box contents
+        self.bench_entry = tk.StringVar()  # var will store bench max text box contents
+        self.squat_entry = tk.StringVar()  # var will store squat max text box contents
+        self.deadlift_entry = tk.StringVar()  # var will store deadlift max text box contents
         self.main_frame = ttk.LabelFrame(self.window, text="Wilkes Coefficient Calculator", relief=tk.RIDGE, padding=6)
-        self.weight_final = ttk.Label(self.window, text="Your Wilks Score:")
-        self.weight_final.grid(row=2, column=1, sticky=tk.W + tk.N)
-        self.radio_val = tk.IntVar()
-        self.create_widgets()
-        self.m_a, self.m_b, self.m_c, self.m_d, self.m_e, self.m_f = -216.047514, 16.260634, -0.002388645, -0.0011373, 7.0186E-06, -1.29E-08
-        self.f_a, self.f_b, self.f_c, self.f_d, self.f_e, self.f_f = 594.3175, -27.238, 0.8212, -0.00931, 4.732E-05, -9.054E-08
+        self.results = ttk.Label(self.window, text="Your Wilks Score:")  # label that will show results
+        self.results.grid(row=2, column=1, sticky=tk.W + tk.N)  # place results underneath everything beside quit button
+        self.radio_val = tk.IntVar()  # radiobutton value stored as either male(0) or female(1)
+        self.create_widgets()  # render each widget instantiated
+        # all the constants - male and female
+        self.m_a, self.m_b, self.m_c = -216.047514, 16.2606339, -0.002388645
+        self.m_d, self.m_e, self.m_f = -0.00113732, 7.01863E-06, -1.291E-08
+        self.f_a, self.f_b, self.f_c = 594.31747775582, -27.23842536447, 0.82112226871
+        self.f_d, self.f_e, self.f_f = -0.00930733913, 4.731582E-05, -9.054E-08
 
     def create_widgets(self):
         # Create some room around all the internal frames
-        self.window['padx'], self.window['pady'] = 4, 4
-
-        self.main_frame = ttk.LabelFrame(self.window, text="Wilkes Calculator", relief=tk.RIDGE, padding=6)
+        self.window['padx'], self.window['pady'] = 4, 4  # spacing around main_frame
+        self.main_frame = ttk.LabelFrame(self.window, text="Wilks Calculator", relief=tk.RIDGE, padding=6)
         self.main_frame.grid(row=1, column=1, padx=6, sticky=tk.E + tk.W + tk.N + tk.S)
 
-        # The Choices
-        self.radio_val.set(0)
+        # The Choices via Radiobutton
+        self.radio_val.set(0)  # default selected button is male
 
-        sex_label = ttk.Label(self.main_frame, width=17, text="Choose your sex")
-        sex_label.grid(row=1, column=1, sticky=tk.W + tk.N)
+        sex_label = ttk.Label(self.main_frame, width=17, text="Choose your sex")  # Label for sex radio buttons
+        sex_label.grid(row=1, column=1, sticky=tk.W + tk.N)  # placed beside radio buttons
 
         male_button = ttk.Radiobutton(self.main_frame, text="Male", variable=self.radio_val, value=0)
         female_button = ttk.Radiobutton(self.main_frame, text="Female", variable=self.radio_val, value=1)
 
-        male_button.grid(row=1, column=2, sticky=tk.W)
-        female_button.grid(row=1, column=2, sticky=tk.E)
+        male_button.grid(row=1, column=2, sticky=tk.W)      # place on same column
+        female_button.grid(row=1, column=2, sticky=tk.E)    # but stuck to different sides
 
         # The Data entries
         weight_label = ttk.Label(self.main_frame, text="Weight (lbs)")
@@ -51,7 +53,7 @@ class WilkesWindow:
         deadlift_label = ttk.Label(self.main_frame, text="Deadlift (lbs)")
         deadlift_label.grid(row=5, column=1, sticky=tk.W + tk.N)
 
-        self.weight_entry = ttk.Entry(self.main_frame, width=25)
+        self.weight_entry = ttk.Entry(self.main_frame, width=25)     # create text boxes, with hints, in main_frame
         self.weight_entry.grid(row=2, column=2, sticky=tk.N, pady=5)
         self.weight_entry.insert(tk.END, "Enter weight here")
 
@@ -67,28 +69,29 @@ class WilkesWindow:
         self.deadlift_entry.grid(row=5, column=2, sticky=tk.N, pady=5)
         self.deadlift_entry.insert(tk.END, "Enter deadlift max here")
 
-        # The Commands
-        calculate_button = tk.Button(self.main_frame, text="Calculate", command=self.on_calc)
-        calculate_button.grid(row=5, column=3, sticky=tk.E, padx=2)
+        # Calculate Button
+        calculate_button = tk.Button(self.main_frame, text="Calculate", command=self.on_calc)  # a click calls on_calc
+        calculate_button.grid(row=5, column=3, sticky=tk.E, padx=2)  # beside last text boxes
 
         # Quit button in the lower right corner
-        quit_button = ttk.Button(self.window, text="Quit", command=self.window.destroy)
+        quit_button = ttk.Button(self.window, text="Quit", command=self.window.destroy)  # calls built-in destroy()
         quit_button.grid(row=2, column=1, sticky=tk.E)
 
     def on_calc(self):
+        # try/except just in case all text boxed don't have integer values
         try:
-            bw = int(self.weight_entry.get()) / 2.205
-            total_lift = (int(self.bench_entry.get()) + int(self.squat_entry.get()) + int(self.deadlift_entry.get())) / 2.205
+            bw = int(self.weight_entry.get()) / 2.20462262185  # body weight conversion from lbs to kg
+            total_lift = (int(self.bench_entry.get()) + int(self.squat_entry.get()) + int(self.deadlift_entry.get())) / 2.20462262185
 
-            if self.radio_val.get() == 0:
+            if self.radio_val.get() == 0:  # if male(0) use male constants
                 denominator = self.m_a + self.m_b * bw + self.m_c * bw ** 2 + self.m_d * bw ** 3 + self.m_e * bw ** 4 + self.m_f * bw ** 5
-            else:
+            else:  # else(1) use female constant
                 denominator = self.f_a + self.f_b * bw + self.f_c * bw ** 2 + self.f_d * bw ** 3 + self.f_e * bw ** 4 + self.f_f * bw ** 5
 
             self.score = total_lift * (500 / denominator)
-            self.weight_final['text'] = "Your Wilkes Score:" + str(self.score)
+            self.results['text'] = f"Your Wilks Score: {str(self.score)} \n[Wilks Coefficient: {500/denominator}]"
         except:
-            self.weight_final['text'] = "Please enter integers into the text boxes above"
+            self.results['text'] = "Please enter integers into ALL text boxes above"
 
 
 # Create the entire GUI program and start the GUI event loop
